eca8f9bdf7edf0f7b2141b2c85598f10bd957f7f
JC <jamescornall@hotmail.co.uk>
2023-06-01 16:41:05 +0100

    git commit -m "Refactor with functions"

--- wordle_clone.py
+++ wordle_clone.py
@@ -2,33 +2,46 @@ import pathlib
 import random
 from string import ascii_letters
 
-WORDLIST = pathlib.Path("wordlist.txt")
+def main():
+    # Pre-process
+    word = get_random_word()
 
-words = [
+    # Process (main loop)
+    for guess_num in range(1, 7):
+        guess = input(f"\nGuess {guess_num}: ").upper()
+
+        show_guess(guess, word)
+        if guess == word:
+            print(f"Correct!\n The word was {word}!")
+            break
+
+    # Post-process
+    else:
+        game_over(word)
+
+def get_random_word():
+    wordlist = pathlib.Path(__file__).parent / "wordlist.txt"
+    words = [
     word.upper()
-    for word in WORDLIST.read_text(encoding="utf-8").split("\n") 
+    for word in wordlist.read_text(encoding="utf-8").split("\n") 
     if len(word) == 5 and all(letter in ascii_letters for letter in word)
-]
+    ]
+    return random.choice(words)
 
-word = random.choice(words)
-
-for guess_num in range(1, 7):
-    guess = input("Guess a word: ").upper()
-    print("Guess " + str(guess_num) + ": " + guess)
-    if guess == word:
-        print("Correct!")
-        break
-
-    correctLetters = {letter for letter, correct in zip(guess, word) if letter == correct}
+def show_guess(guess, word):
+    correctLetters = {
+        letter for letter, correct in zip(guess, word) if letter == correct
+    }
     misplacedLetters = set(guess) & set(word) - correctLetters
     wrongLetters = set(guess) - set(word)
 
     print("Correct Letters:", ", ".join(sorted(correctLetters)))
     print("Misplaced Letters:", ", ".join(sorted(misplacedLetters)))
     print("Wrong Letters:", ", ".join(sorted(wrongLetters)))
+     
+def game_over(word):
+    print(f"\n--- Game Over ---")
+    print(f"The word was {word}")
 
-    if guess_num == 6:
-            print("Game Over")
-            print(f"The word was {word}")
-
-
+if __name__ == "__main__":
+    main()
