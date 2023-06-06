0bfd5ac8354dde0af15d34cf97a3ad118c5900c5
JC <jamescornall@hotmail.co.uk>
2023-06-06 13:48:39 +0100

    Refactor functions and add doctests

--- wordle_clone.py
+++ wordle_clone.py
@@ -4,7 +4,8 @@ from string import ascii_letters
 
 def main():
     # Pre-process
-    word = get_random_word()
+    words_path = pathlib.Path(__file__).parent / "wordlist.txt"
+    word = get_random_word(words_path.read_text(encoding="utf-8").split("\n"))
 
     # Process (main loop)
     for guess_num in range(1, 7):
@@ -19,16 +20,33 @@ def main():
     else:
         game_over(word)
 
-def get_random_word():
-    wordlist = pathlib.Path(__file__).parent / "wordlist.txt"
+def get_random_word(word_list):    
+    """Selects a random five-letter word from the wordlist.txt file.
+    
+    ## Example:
+    
+    >>> get_random_word(["broke", "stop", "kn1ght'5"])
+    'BROKE'
+    """
+
     words = [
-    word.upper()
-    for word in wordlist.read_text(encoding="utf-8").split("\n") 
-    if len(word) == 5 and all(letter in ascii_letters for letter in word)
+        word.upper()
+        for word in word_list
+        if len(word) == 5 and all(letter in ascii_letters for letter in word)
     ]
     return random.choice(words)
 
 def show_guess(guess, word):
+    """Display user's guess on the terminal and classify letters (i.e. correct, misplaced, wrong)
+    
+    ## Example:
+    
+    >>> show_guess("ANGEL", "CRANE")
+    Correct Letters: 
+    Misplaced Letters: A, E, N
+    Wrong Letters: G, L
+    """
+
     correctLetters = {
         letter for letter, correct in zip(guess, word) if letter == correct
     }
@@ -40,6 +58,16 @@ def show_guess(guess, word):
     print("Wrong Letters:", ", ".join(sorted(wrongLetters)))
      
 def game_over(word):
+    """Display the 'Game Over' message and the random word.
+    
+    ## Example:
+    
+    >>> game_over("ANGEL")
+    <BLANKLINE>
+    --- Game Over ---
+    The word was ANGEL
+    """
+
     print(f"\n--- Game Over ---")
     print(f"The word was {word}")
 
