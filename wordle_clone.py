a3ae86128119f4e9318e7f44501724edef0be1d0
JC <jamescornall@hotmail.co.uk>
2023-06-08 16:43:01 +0100

    Refactor variables and add letter overview

--- wordle_clone.py
+++ wordle_clone.py
@@ -1,26 +1,32 @@
+import contextlib
 import pathlib
 import random
-from string import ascii_letters
+from string import ascii_letters, ascii_uppercase
 
 from rich.console import Console
 from rich.theme import Theme
 
 console = Console(width=40, theme=Theme({"warning": "red on yellow"}))
 
+NUM_LETTERS = 5
+NUM_GUESSES = 6
+WORDS_PATH = pathlib.Path(__file__).parent / "wordlist.txt"
+
 def main():
     # Pre-process
     words_path = pathlib.Path(__file__).parent / "wordlist.txt"
     word = get_random_word(words_path.read_text(encoding="utf-8").split("\n"))
-    guesses = ["_" * 5] * 6
+    guesses = ["_" * NUM_LETTERS] * NUM_GUESSES
 
     # Process (main loop)
-    for index in range(6):
-        refresh_page(headline=f" Guess {index + 1}")
-        show_guesses(guesses, word)
+    with contextlib.suppress(KeyboardInterrupt):
+        for index in range(NUM_GUESSES):
+            refresh_page(headline=f" Guess {index + 1}")
+            show_guesses(guesses, word)
 
-        guesses[index] = guess_word(previous_guesses=guesses[:index])
-        if guesses[index] == word:
-            break
+            guesses[index] = guess_word(previous_guesses=guesses[:index])
+            if guesses[index] == word:
+                break
 
     # Post-process
     game_over(guesses, word, guessed_correctly=guesses[index] == word)
@@ -56,7 +62,7 @@ def get_random_word(word_list):
     if words := [
         word.upper()
         for word in word_list
-        if len(word) == 5 and all(letter in ascii_letters for letter in word)
+        if len(word) == NUM_LETTERS and all(letter in ascii_letters for letter in word)
     ]:
         return random.choice(words)
     else:
@@ -64,20 +70,24 @@ def get_random_word(word_list):
         raise SystemExit()
 
 def show_guesses(guesses, word):
+    letter_status = {letter: letter for letter in ascii_uppercase}
     for guess in guesses:
-            styled_guess = []
-            for letter, correct in zip(guess,word):
-                if letter == correct:
-                    style = "bold white on green"
-                elif letter in word:
-                    style = "bold white on yellow"
-                elif letter in ascii_letters:
-                    style = "white on #666666"
-                else:
-                    style = "dim"
-                styled_guess.append(f"[{style}]{letter}[/]")
+        styled_guess = []
+        for letter, correct in zip(guess,word):
+            if letter == correct:
+                style = "bold white on green"
+            elif letter in word:
+                style = "bold white on yellow"
+            elif letter in ascii_letters:
+                style = "white on #666666"
+            else:
+                style = "dim"
+            styled_guess.append(f"[{style}]{letter}[/]")
+            if letter != "_":
+                letter_status[letter] = f"[{style}]{letter}[/]"
         
-            console.print("".join(styled_guess), justify="center")
+        console.print("".join(styled_guess), justify="center")
+    console.print("\n" + "".join(letter_status.values()), justify="center")
 
 ## Deprecated version of the show_guess function
 # def show_guess(guess, word):
